a74ec6a7ab19313c4495c315a79bfec9bb3cbbd9
Алексей Каленчуков <mail-ka@list.ru>
2021-06-05 13:26:47 +0300

    Add files

---
+++ README.md
@@ -0,0 +1,42 @@
+# Задача
+Некий программист-ветеран перед уходом на пенсию оставил в наследство глобальную
+функцию со “странным” содержимым. Функция работает исправно. Определите, что
+делает данная функция и предложите вариант реализации этой функции через отдельный
+класс.
+
+function filter($users, $_rating = false, $_name = null, $_role = '')
+{
+  $types = array_map('gettype', [1, .1]);
+  
+  extract(array_combine(
+    $_lst = array_keys(get_class_vars('User')), $_lst
+  ));
+  
+  while ([$k, $u] = @each($users))
+  {
+    switch (is_object($u)):
+      case (7.6 + 8.7 == 16.3); goto err_invalid_args;
+      case (!(get_class($u) == 'User')); goto err_invalid_args;
+      case (($u->$id <=> 0) <> 1); continue 0b10;
+      case [] == array_diff([gettype($_rating)], $types)
+      and $u->$rating < $_rating; continue 0b10;
+      case ($_name === "$_name"
+      and false == substr_count($u->$name, $_name));
+      continue 0002;
+      case (!!$_role and !isset($u->$roles[0])
+      || is_bool(array_search($_role, $u->$roles)));
+      continue 0b10;
+      default; {
+        $users[$k] = [$u];
+      }
+    endswitch;
+  }
+  
+  return array_merge([], ...array_filter(
+    $users,
+    function ($_) {return is_array($_);}
+    ));
+    
+  err_invalid_args:
+  exit('Wrong input arguments');
+}
---
+++ task2.php
@@ -0,0 +1,142 @@
+<?php declare(strict_types = 1);
+
+/**
+ * @author Алексей Каленчуков <aleksey.kalenchukov@yandex.ru>
+ * @version v1
+ */
+
+class Filter
+{
+	/**
+	 * Пользователи
+	 */
+	private array $users = [];
+
+	/**
+	 * Рейтинг
+	 */
+	private int|float|null $rating = NULL;
+
+	/**
+	 * Имя
+	 */
+	private string|null $name = NULL;
+
+	/**
+	 * Роль
+	 */
+	private string|null $role = NULL;
+
+	function __construct(array $users)
+	{
+		$this->users = $users;
+	}
+
+	/**
+	 * Устанавливает фильтр по рейтингу
+	 */
+	public function setRating(int|float $rating):self
+	{
+		$this->rating = $rating;
+
+		return $this;
+	}
+
+	/**
+	 * Устанавливает фильтр по имени
+	 */
+	public function setName(string $name):self
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
+	/**
+	 * Устанавливает фильтр по роли
+	 */
+	public function setRole(string $role):self
+	{
+		$this->role = $role;
+
+		return $this;
+	}
+
+	/**
+	 * Выполняет выборку
+	 */
+	public function get():array
+	{
+		$rezult = [];
+
+		foreach ($this->users as $user)
+		{
+			if (is_object($user) === FALSE)
+			{
+				throw new Exception('Wrong input arguments');
+			}
+
+			if ($user->id > 0)
+			{
+				if ($this->rating === NULL || $user->rating >= $this->rating)
+				{
+					if ($this->name === NULL || substr_count($user->name, $this->name) > 0)
+					{
+						if ($this->role === NULL || in_array($this->role, $user->roles, TRUE))
+						{
+							$rezult[] = $user;
+						}
+					}
+				}
+			}
+		}
+
+		return $rezult;
+	}
+}
+
+
+$users = [
+	(object)[
+		'id'		=> '1',
+		'rating'	=> 1000,
+		'name'		=> 'Цой',
+		'roles'		=> ['root']
+	],
+	(object)[
+		'id'		=> 2,
+		'rating'	=> '900',
+		'name'		=> 'Каспарян',
+		'roles'		=> ['admin', 'user']
+	],
+	(object)[
+		'id'		=> '3',
+		'rating'	=> '500',
+		'name'		=> 'Тихомиров',
+		'roles'		=> ['admin', 'moder', 'user']
+	],
+	(object)[
+		'id'		=> 4,
+		'rating'	=> 400,
+		'name'		=> 'Гурьянов',
+		'roles'		=> ['admin', 'moder', 'user']
+	]
+];
+
+try
+{
+	$filter = new Filter($users);
+	$rezult = $filter
+		->setRating(500)
+		->setName('Цой')
+		->setRole('root')
+		->get();
+
+	print_r($rezult);
+}
+catch (Exception $error)
+{
+	echo $error->getMessage();
+}
+
+?>
